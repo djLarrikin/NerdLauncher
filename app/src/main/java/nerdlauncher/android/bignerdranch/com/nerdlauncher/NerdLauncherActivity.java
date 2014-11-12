e303a5e6c9ac986c826931aaa9a0dc1b204dbf01
Jonathan Morton <djlarrikin@gmail.com>
2014-11-12 17:08:40 +0900

    Chapter 23 completed with the challenge

--- app/src/main/java/nerdlauncher/android/bignerdranch/com/nerdlauncher/NerdLauncherActivity.java
+++ app/src/main/java/nerdlauncher/android/bignerdranch/com/nerdlauncher/NerdLauncherActivity.java
@@ -5,11 +5,14 @@ import android.content.pm.PackageManager;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class NerdLauncherActivity extends SingleFragmentActivity {
+
+    String TAG = "NerdLauncherActivity";
 
     @Override
     public Fragment createFragment(){
--- app/src/main/java/nerdlauncher/android/bignerdranch/com/nerdlauncher/NerdLauncherFragment.java
+++ app/src/main/java/nerdlauncher/android/bignerdranch/com/nerdlauncher/NerdLauncherFragment.java
@@ -1,12 +1,23 @@
 package nerdlauncher.android.bignerdranch.com.nerdlauncher;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -19,12 +30,59 @@ public class NerdLauncherFragment extends ListFragment {
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
+        Log.i(TAG, "OnCreate started");
         Intent startupIntent = new Intent(Intent.ACTION_MAIN);
         startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
 
-        PackageManager pm = getActivity().getPackageManager();
+        final PackageManager pm = getActivity().getPackageManager();
         List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
 
         Log.i(TAG, "I've found " + activities.size() + " activities");
+
+        Collections.sort(activities, new Comparator<ResolveInfo>() {
+            @Override
+            public int compare(ResolveInfo a, ResolveInfo b) {
+                PackageManager pm = getActivity().getPackageManager();
+                return String.CASE_INSENSITIVE_ORDER.compare(a.loadLabel(pm).toString(), b.loadLabel(pm).toString());
+            }
+        });
+
+        NerdLauncherAdapter adapter = new NerdLauncherAdapter(activities);
+
+        setListAdapter(adapter);
+    }
+
+    private class NerdLauncherAdapter extends ArrayAdapter<ResolveInfo>{
+        public NerdLauncherAdapter(List<ResolveInfo> resolveInfos){
+            super(getActivity(), 0, resolveInfos);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent){
+            if (convertView == null){
+                convertView = getActivity().getLayoutInflater().inflate(R.layout.list_item_app, null);
+            }
+            final PackageManager pm = getActivity().getPackageManager();
+            ResolveInfo ri = getItem(position);
+            TextView tv = (TextView)convertView.findViewById(R.id.text_view);
+            tv.setText(ri.loadLabel(pm));
+            ImageView cv = (ImageView)convertView.findViewById(R.id.circle_image_view);
+            cv.setBackground(ri.loadIcon(pm));
+            return convertView;
+        }
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id){
+        ResolveInfo resolveInfo = (ResolveInfo)l.getAdapter().getItem(position);
+        ActivityInfo activityInfo = resolveInfo.activityInfo;
+
+        if (activityInfo == null) return;
+
+        Intent i = new Intent(Intent.ACTION_MAIN);
+        i.setClassName(activityInfo.applicationInfo.packageName, activityInfo.name);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        startActivity(i);
     }
 }
